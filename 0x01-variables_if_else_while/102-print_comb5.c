97143106e6c25657942471980cce24f37eaf20c9
Ebisine <ebisglam@gmail.com>
2022-09-10 11:56:38 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,34 +1,50 @@
 #include <stdio.h>
 /**
- * main - print if the number is postive, zero, or negative
- *
- * Description: using the main function
- * this program prints "Programming is positive, zero, or negative
+ * main - main block
+ * Description: Print all possible combinations of two two-digit numbers
+ * Numbers should range from 0 to 99
+ * The two numbers should be separated by a space
+ * All numbers should be printed with two digits. 1 should be printed as 01
+ * Combination of numbers must be separated by a comma followed by a space
+ * Combinations of numbers should be printed in ascending order
+ * `00 01` and `01 00` are considered as the same combination
+ * You can only use `putchar` to print to console
+ * You can only use `putchar` up to 8 times
+ * You are not allowed to use any variable of type `char`
  * Return: 0
  */
 int main(void)
 {
-	int i, p;
+	int i, j;
+	int a, b, c, d;
 
 	for (i = 0; i < 100; i++)
 	{
-		for (p = 0; p < 100; j++)
+		a = i / 10; /* doubles fnum */
+		b = i % 10; /* singles fnum */
+		
+		for (j = 0; j < 100; j++)
 		{
-			if (p > i)
+			c = j / 10; /* doubles snum */
+			d = j % 10; /* singles snum */
+
+			if (a < c || (a == c && b < d))
 			{
-				putchar((i / 10) + '0');
-				putchar((i % 10) + '0');
-				putchar(' ');
-				putchar((p / 10) + '0');
-				putchar((p % 10) + '0');
-				if (i != 98)
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(32);
+				putchar(c + '0');
+				putchar(d + '0');
+		
+				if (!(a == 9 && b == 8))
 				{
-					putchar(',');
-					putchar(' ');
+					putchar(44);
+					putchar(32);
 				}
 			}
 		}
 	}
-	putchar('\n');
+	putchar(10);
+	
 	return (0);
 }
